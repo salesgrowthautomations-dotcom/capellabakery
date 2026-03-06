319191a2cfc6308d006b61ff32d3b99b81a98be4
salesgrowthautomations-dotcom <salesgrowthautomations@gmail.com>
2026-03-06 20:50:14 +0530

    Premium Capella bakery design

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Capella Bakery Website Loaded");
+document.querySelectorAll("a").forEach(link => {
+link.addEventListener("click", function(e){
+const href = this.getAttribute("href");
+if(href.startsWith("#")){
+e.preventDefault();
+document.querySelector(href).scrollIntoView({
+behavior:"smooth"
+});
+}
+});
+});
